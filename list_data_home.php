c86ed33bff8567602051e9d728e4f5349ebbd959
zapo1234 <52051952+zapo1234@users.noreply.github.com>
2021-06-02 23:39:36 +0200

    Add files via upload

--- list_data_home.php
+++ list_data_home.php
@@ -3,10 +3,16 @@ include('connecte_db.php');
 include('inc_session.php');
 
    
-   // emttre la requete sur le fonction
-    $req=$bds->prepare('SELECT id,id_chambre,chambre,type_logement,equipements,equipement,cout_nuite,cout_pass,icons,infos FROM chambre WHERE email_ocd= :email_ocd');
+    // emttre la requete sur le fonction
+    $req=$bds->prepare('SELECT id,id_chambre,chambre,type_logement,equipements,equipement,cout_nuite,cout_pass,icons,infos FROM chambre WHERE email_ocd= :email_ocd LIMIT 0,12');
     $req->execute(array(':email_ocd'=>$_SESSION['email_ocd']));
 	
+	$don = $req->fetchAll();
+	
+    //// emttre la requete sur le fonction
+    $rec=$bds->query('SELECT id_chambre FROM home_occupation');
+    $donns = $rec->fetchAll();
+	$rec->closeCursor();
 	
 	$rem='<span class="ts"></span>';
 	$rt=",";
@@ -16,21 +22,39 @@ include('inc_session.php');
        
 	echo'<div class="content_home">';
 	
-	while($donnees = $req->fetch()){
+	$tab = [];
+	// on récupére les données
+	
+	foreach($don as $donnees){
 		
-		echo'<div class="content3">
-		     <span class="dc">Type de local :'.$donnees['type_logement'].'</span><br/><span class="df">'.$donnees['chambre'].'</span><br/>
-			 <span class="dt">'.str_replace($rt,$rem,$donnees['equipement']).'</span><br/><span class="text">Prix négocié(nuité)<input type="text" name="count_nuite" class="prix" id="prix'.$donnees['cout_nuite'].'"></span>
-			 <span class="tex">Prix négocié(horaire)<input type="text" name="count_pass" class="pric" id="pric'.$donnees['cout_nuite'].'"></span><br/><br/>
-			 <span class="but1"><a href="#" data-id1="'.$donnees['id_chambre'].'" title="voir disponibilité">check</a></span> <span id="d'.$donnees['id_chambre'].'"></span> <span class="but2"><a href="#" data-id="'.$donnees['id_chambre'].'" title="facturé le local">Ajouter le local</a></span>
-			 <input type="hidden"  name="hidden_name" id="chambre'.$donnees['id_chambre'].'" value="'.$donnees['chambre'].'">
-			 <input type="hidden" id="cout_nuit'.$donnees['id_chambre'].'" value="'.$donnees['cout_nuite'].'">
-		     <input type="hidden" id="cout_pass'.$donnees['id_chambre'].'" value="'.$donnees['cout_pass'].'">
-			 <input type="hidden" id="chambre'.$donnees['id_chambre'].'" value="'.$donnees['chambre'].'">
-			
-			 </div>';
+		$datas = $donnees['id_chambre'];
+		
+		$datas = explode(' ',$datas);
+		
+		foreach($datas as $values){
+		
+        $tab[] = $values;		
+	   }
 	}
 	
+	
+	
+	
+	foreach($don as $donnees) {
+		
+	  echo'<div class="content3">
+		     <span class="dc">Type de local :'.$donnees['type_logement'].'</span><br/><span class="df">'.$donnees['chambre'].'</span><br/>
+			 <span class="dt">'.str_replace($rt,$rem,$donnees['equipement']).'</span><br/><span class="text">Prix négocié(nuité)<input type="number"  class="prix" id="prix'.$donnees['id_chambre'].'"></span>
+			 <span class="tex">Prix négocié(horaire)<input type="number"  class="pric" id="pric'.$donnees['id_chambre'].'"></span><br/><br/>
+			 <span class="but1"><a href="#" data-id1="'.$donnees['id_chambre'].'" title="voir disponibilité">check</a></span> <span id="d'.$donnees['id_chambre'].'"></span><span class="but2"><a href="#" class="add_home" data-id2="'.$donnees['id_chambre'].'" title="facturé le local">Ajouter le local</a></span>
+			 <input type="hidden"  name="hidden_name" id="chambre'.$donnees['id_chambre'].'" value="'.$donnees['chambre'].'">
+			 <input type="hidden" id="cout_nuite'.$donnees['id_chambre'].'" value="'.$donnees['cout_nuite'].'">
+		     <input type="hidden" id="cout_pass'.$donnees['id_chambre'].'" value="'.$donnees['cout_pass'].'">
+			 <input type="hidden" id="chambre'.$donnees['id_chambre'].'" value="'.$donnees['chambre'].'">
+			<input type="hidden" id="type'.$donnees['id_chambre'].'" value="'.$donnees['type_logement'].'">
+			 </div>';
+	
+	}
 	
 	
 	echo'</div>';
