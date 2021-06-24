43372c5bd4e9cab6d58f2a647607940fe32e38ff
zapo1234 <52051952+zapo1234@users.noreply.github.com>
2021-06-24 17:38:36 +0200

    Add files via upload

--- depenses_validate_datas.php
+++ depenses_validate_datas.php
@@ -2,72 +2,7 @@
 include('connecte_db.php');
 include('inc_session.php');
 
- 
- if($_POST['action']=="fetchs") {
-	 
- $req=$bds->prepare('SELECT id,date,designation,fournisseur,montant,user,status FROM depense WHERE email_ocd= :email_ocd');
- $req->execute(array(':email_ocd'=>$_SESSION['email_ocd']));
- $datas=$req->fetchAll();
-	
-  // on boucle sur les les resultats
-	echo'<div class="expor"><h2>Gestion des dépenses de votre activité</h2> <span class="export">Export  <button type="button" class="excel">Excel <i class="far fa-file-excel"></i></button>
-	<button type="button" class="csv">Csv <i class="fas fa-file-csv"></i></button><span></div>';
-  echo'	<table class="table">
-     <thead>
-     <tr class="tf">
-	  <th scope="col">Date</th>
-	  <th scope="col">N°facture(si existe)</th>
-      <th scope="col">Désignation</th>
-	  <th scope="col">fournisseur</th>
-	  <th scope="col">Gestionnaire</th>
-	  <th scope="col">Montant</th>
-	  <th scope="col">Actions</th>
-      </tr>
-      </thead>
-      <tbody>';
-  
-  foreach($datas as $donnes){
 
-    $nombre =$donnes['status'];
-	
-	if($donnes['status']==1){
-	 $name="dépense effectuée";
-	 $mettre ='<a href="#" class="mettre" title="mettre à jour" data-id1='.$donnes['id'].'"><i class="fab fa-telegram"></i>Mettre à jour</a><br/>';
-	}
-	elseif($donnes['status']==2){
-	 $name="crédit fournisseur";
-	 $mettre='<a href="#" class="mettre" title="mettre à jour" data-id1='.$donnes['id'].'"><i class="fab fa-telegram"></i>Mettre à jour</a><br/>';
-	}
-	
-	else{
-		
-		
-	}
-
-   $date1=$donnes['date'];
-	$date1 = explode('-',$date1);
-	$j = $date1[2];
-	$mm = $date1[1];
-	$an = $date1[0];
-	
-	echo'<tr class="datas'.$donnes['status'].'">
-	     <td><span class="dat'.$donnes['status'].'"><i class="fas fa-circle" style="font-size:10px;"></i></span><span class="der"> Facture émise le<br/>'.$j.'/'.$mm.'/'.$an.'<br/>
-		 '.$donnes['user'].'</span></td>
-		 <td><span class="der">N° facture: '.$donnes['numero_facture'].'<br/><div class="data'.$donnes['status'].'">'.$name.'</span></div></td>
-		 <td><span class="repas">'.$donnes['designation'].'</td>
-		 <td><span class="repas">'.$donnes['fournisseur'].'</td>
-		 <td><span class="repas">'.$donnes['user'].'</td>
-		 <td><span class="repas">'.$donnes['montant'].'xof</td>
-		 <td> Action à gérer <span class="action" data-id2="'.$donnes['id'].'"><i class="fas fa-angle-down"></i></span><div class="datas" style="display:none" id="content'.$donnes['id'].'">
-		 '.$mettre.'
-		  <a href="#" class="annuler" title="Annuler" data-id2='.$donnes['id'].'"><i class="fab fa-telegram"></i>Annuler</a><br/>
-		  </div></td>
-		 <td><a href="gestion_datas_facture.php?id_fact="'.$donnes['id'].'"><i class="far fa-file-pdf" style="color:red;font-size:16px;"></i></a></td>
-	    </tr>';
-    }	  
-  }
-
- if($_POST['action']=="deleted"){
  // on recupere le montant
  $req=$bds->prepare('SELECT depense FROM tresorie_customer WHERE email_ocd= :email_ocd');
  $req->execute(array(':email_ocd'=>$_SESSION['email_ocd']));
@@ -80,6 +15,7 @@ include('inc_session.php');
  $nature=$_POST['des'];
  $monts=$_POST['idy'];
  $user =$_SESSION['user'];
+ $numero_facture= html_entity_decode($_POST['fact']);
 
  for($count=0;  $count<count($_POST['ti']); $count++){
 	
@@ -98,13 +34,10 @@ $natu='dépense effectué';
 	$natu='crédit fournisseur'; 
  }
  
- 
-    echo'<div class="enre"><div>vos dépenses sont bien enregistrées 
-		     <div class="dep"><i style="font-size:40px;color:green" class="fa">&#xf250;</i></div></div>';
- 
  // insertions des données dans la base de données depense
- $req=$bds->prepare('INSERT INTO depense(email_ocd,date,designation,fournisseur,user,nature,montant,status) VALUES(:email_ocd,:date,:designation,:fournisseur,:user,:nature,:montant,:status)');
+ $req=$bds->prepare('INSERT INTO depense(email_ocd,numero_facture,date,designation,fournisseur,user,nature,montant,status) VALUES(:email_ocd,:numero_facture,:date,:designation,:fournisseur,:user,:nature,:montant,:status)');
  $req->execute(array(':email_ocd'=>$_SESSION['email_ocd'],
+                     ':numero_facture'=>$numero_facture,
                       ':date'=>$dates,
 					  ':designation'=>$desi,
 					  ':fournisseur'=>$fourni,
@@ -125,7 +58,7 @@ $natu='dépense effectué';
                        ':email_ocd'=>$_SESSION['email_ocd']
 					 ));
 					 
-  }	
+  
   
 
 ?>
---
+++ depenses_view_datas.php
@@ -0,0 +1,72 @@
+<?php
+include('connecte_db.php');
+include('inc_session.php');
+
+
+if($_POST['action']=="fetchs") {
+	 
+ $req=$bds->prepare('SELECT id,date,designation,fournisseur,montant,user,status,numero_facture FROM depense WHERE email_ocd= :email_ocd');
+ $req->execute(array(':email_ocd'=>$_SESSION['email_ocd']));
+ $datas=$req->fetchAll();
+	
+  // on boucle sur les les resultats
+	echo'<div class="expor"><h2>Gestion des dépenses de votre activité</h2> <span class="export">Export  <button type="button" class="excel">Excel <i class="far fa-file-excel"></i></button>
+	<button type="button" class="csv">Csv <i class="fas fa-file-csv"></i></button><span></div>';
+  echo'	<table class="table">
+     <thead>
+     <tr class="tf">
+	  <th scope="col">Date</th>
+	  <th scope="col">N°facture(si existe)</th>
+      <th scope="col">Désignation</th>
+	  <th scope="col">fournisseur</th>
+	  <th scope="col">Gestionnaire</th>
+	  <th scope="col">Montant</th>
+	  <th scope="col">Actions</th>
+      </tr>
+      </thead>
+      <tbody>';
+  
+  foreach($datas as $donnes){
+
+    $nombre =$donnes['status'];
+	
+	if($donnes['status']==1){
+	 $name="dépense effectuée";
+	 $mettre ='<a href="#" class="mettre" title="mettre à jour" data-id3='.$donnes['id'].'"><i class="fab fa-telegram"></i>Mettre à jour</a><br/>';
+	 $annul='<a href="#" class="modifier" title="modifier" data-id4='.$donnes['id'].'"><i class="fab fa-telegram"></i>Annuler</a><br/>';
+	}
+	elseif($donnes['status']==2){
+	 $name="crédit fournisseur";
+	 $mettre='<a href="#" class="mettre" title="mettre à jour" data-id3='.$donnes['id'].'"><i class="fab fa-telegram"></i>Mettre à jour</a><br/>';
+	 $annul='<a href="#" class="modifier" title="modifier" data-id4='.$donnes['id'].'"><i class="fab fa-telegram"></i>Annuler</a><br/>';
+	}
+	
+	else{
+	$name="dépense annulée";
+	$mettre="";
+	$annul="";
+	}
+
+   $date1=$donnes['date'];
+	$date1 = explode('-',$date1);
+	$j = $date1[2];
+	$mm = $date1[1];
+	$an = $date1[0];
+	
+	echo'<tr class="datas'.$donnes['status'].'">
+	     <td><span class="dat'.$donnes['status'].'"><i class="fas fa-circle" style="font-size:10px;"></i></span><span class="der"> enregistrement effectué le<br/>'.$j.'/'.$mm.'/'.$an.'<br/>
+		 '.$donnes['user'].'</span></td>
+		 <td><span class="der">N° facture: '.$donnes['numero_facture'].'<br/><div class="data'.$donnes['status'].'">'.$name.'</span></div></td>
+		 <td><span class="repas">'.$donnes['designation'].'</td>
+		 <td><span class="repas">'.$donnes['fournisseur'].'</td>
+		 <td><span class="repas">'.$donnes['user'].'</td>
+		 <td><span class="repas">'.$donnes['montant'].'xof</td>
+		 <td> Action à gérer <span class="action" data-id2="'.$donnes['id'].'"><i class="fas fa-angle-down"></i></span><div class="datas" style="display:none" id="content'.$donnes['id'].'">
+		 '.$mettre.'
+		  <a href="#" class="modifier" title="modifier" data-id3='.$donnes['id'].'"><i class="fab fa-telegram"></i>modifier</a><br/>
+		 '.$annul.'
+		  
+		  </div></td>
+	    </tr>';
+    }	  
+  }
