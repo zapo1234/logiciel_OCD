48a9750837d83c2b09162d0a2cc926b305ba2e69
zapo1234 <52051952+zapo1234@users.noreply.github.com>
2021-07-22 15:36:18 +0200

    Add files via upload

--- generate_data_pdf.php
+++ generate_data_pdf.php
@@ -21,11 +21,40 @@
     <!-- Custom styles for this page -->
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
    <style type="text/css">
-    .content2{width:100%;height:200px;border:1px solid #eee;} 
-    table{margin-top:50px;} .cont1,.conten1{float:left;} #logo{width:140px;height:140px;border-radius:100px;background:white;border:2px solid #eee;} .cont1{position:absolute;top:100px;left:65%;}
-	.content3{margin-top:40px;} 
+    .content2{width:100%;height:200px;border:1px solid #eee;} .conten1{font-size:14px;} 
+     .cont1,.conten1{float:left;} #logo{width:140px;height:140px;border-radius:100px;background:white;border:2px solid #eee;} 
+	 .cont1{position:absolute;top:100px;left:60%;background:#eee;width:260px;height:180px;border:1px solid #eee;padding-left:30px;padding-top:20px;}
+	.content3{margin-top:40px;} .cont1{font-size:15px;}
 	
+	.number{font-family:arial;font-size:20px;text-transform:uppercase;}
+	.dr{font-family:arial;font-size:15px;padding-left:3%;}
+    .dv{padding-left:7%;} 
+	
+	#tb {font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;
+	margin-top:50px;}
 
+#tb td, #tb th {border: 1px solid #ddd;padding: 8px;width:150px;text-align:center;font-size:14px;}
+
+#tb tr:nth-child(even){background-color: #f2f2f2;}
+
+#tb tr:hover {background-color: #ddd;}
+
+#tb th {padding-top: 12px;padding-bottom: 12px;text-align: left;color: black;text-align:center;background:#D2EDF9;border:2px solid #D2EDF9}
+
+ 
+#tab {font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 100%;
+	margin-top:55px;margin-left:63%;}
+
+#tab td, #tab th {border: 1px solid #ddd;padding: 8px;width:100px;text-align:center;font-size:14px;}
+
+#tab tr:nth-child(even){background-color: #f2f2f2;}
+
+#tab tr:hover {background-color: #ddd;}
+
+#tab th {padding-top: 12px;padding-bottom: 12px;text-align: left;color: black;text-align:center;}
+
+ 
+	
    </style>
 
   <?php
@@ -45,18 +74,38 @@
                       ':email_ocd'=>$_SESSION['email_ocd']));
    $donns=$res->fetch();
    
+   $dates2 = explode('-',$donns['check_in']);
+	
+	$j = $dates2[2];
+	$mm = $dates2[1];
+	$an = $dates2[0];
+	//
+	$dat=$j.'/'.$mm.'/'.$an.'';
+	
+	$dates1 = explode('-',$donns['check_out']);
+	
+	$j1 = $dates1[2];
+	$mm1 = $dates1[1];
+	$an1 = $dates1[0];
+   //
+	$dat1=$j1.'/'.$mm1.'/'.$an1.'';
    // éditer la facture client
    if($donns['type']==1 OR $donns['type']==3) {
 	$date1 = $donns['check_in'];
     $date2 =$donns['check_out'];
-    $date = '.Date d\'entrée:'.$date1.' Date de sortie:'.$date2;	
+    $date = '.Date d\'entrée:'.$dat.' Date de sortie:'.$dat1.'';
+     $time = 'Cout/nuité';
+     $time1 ='Nombre/jour(s)';	 
 	}
    
    if($donns['type']==2){
 	$date1 = $donns['time'];
-    $date2 =$donns['time1'];
-	$date = '.Heure d\'entrée:'.$date1.' Heure de sortie:'.$date2;	
+    $date2 = $donns['time1'];
+	$date = '.Heure d\'entrée:'.$dat.' Heure de sortie:'.$dat1;	
+	$time ='Cout/horaire';
+	$time1='Nombre/d\'heure(s)';
    }
+   
    
    echo'<div class="content2">
         
@@ -64,42 +113,45 @@
 		<br/><br/><span class="entre">Entreprise '.$donnees['denomination'].'</span><br/><br/>
 		<span class="tel">Numéro tel '.$donnees['numero'].'</span><br/><br/>
 		<span class="tel">Email '.$donnees['email_ocd'].'</span><br/><br/>
-		<span class="adresse">Adresse '.$donnees['adresse'].'</span>
+		<span class="adresse">Adresse '.$donnees['adresse'].'</span><br/><br/><br/>
+		<span class="number">N° facture:<strong>'.$_GET['id_fact'].'</strong></span><br/>
 		</div>
 		
 		<div class="cont1" style="float:left">
-	     <span class="fact">Date d\'édition:'.$donns['date'].'</span><br/><br/>
-		<span class="name"> Nom client:'.$donns['clients'].'</span><br/><br/>
-		<span class="name"> Numéro:'.$donns['numero'].'</span><br/>
-		<span class="email">Email:'.$donns['email_client'].'</span><br/>
+	     <span class="fact">Date d\'édition:'.$donns['date'].'</span><br/><br/>
+		<span class="name">Nom client:<br/>'.$donns['clients'].'</span><br/><br/>
+		<span class="name">Numéro:'.$donns['numero'].'</span><br/><br/>
+		<span class="email">Email:'.$donns['email_client'].'</span><br/><br/>
 		
 		</div><!--cont1-->
 		
 	     </div>';
 		
 	echo'<div class="content3">
-	     <div class="dr">Type : '.$donns['types'].' <div class="dr">'.$date.'</div></div>
-	     <table class="tb">
+	     <div class="dr"> Type : '.$donns['types'].' <span class="dv">'.$date.'</span></div>
+	     <table id="tb">
 		 <tr>
 		 <th>Désignation du local</th>
-		 <th>Cout(Nuité/horaire)</th>
-		 <th>Nombre(jour/heure</th>
+		 <th>'.$time.'</th>
+		 <th>'.$time1.'</th>
 		 <th>Total à payer</th>
 		 </tr>';
 		 
 		 while($donnees =$reg->fetch()){
+			 
+			$montants =$donnees['montant']*$donns['nombre']; 
 		 echo'<tr>
              <td>'.$donnees['chambre'].'</td>
 			 <td>'.$donnees['montant'].'</td>
 			 <td>'.$donns['nombre'].'</td>
-			 <td>'.$donnees['montant'].'</td>
+			 <td>'.$montants.'</td>
              </tr>';		 
 		}
 		 
 		echo'</table>';
 		
 		$montant=$donns['montant']-floatval($donns['mont_tva']);
-		echo'<table class="tab">
+		echo'<table id="tab">
 		     <tr>
 			 <td>Montant(repas)</td>
 			 <td>'.$donns['montant_repas'].'xof</td>
