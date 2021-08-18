0f6df136b8b5c57d737eb95fcc345456ff3b3462
zapo1234 <52051952+zapo1234@users.noreply.github.com>
2021-08-18 23:39:38 +0200

    Add files via upload

--- result_facture_home.php
+++ result_facture_home.php
@@ -35,7 +35,7 @@ $smart_from =($page -1)*$record_peage;
 		}
 	
 		// emttre la requete sur le fonction
-    $req=$bds->prepare('SELECT  date,adresse,check_in,check_out,time,time1,clients,user,montant,montant_repas,mont_tva,types,id_fact,nombre,type,society FROM facture WHERE email_ocd= :email_ocd AND code= :code ORDER BY id_fact DESC LIMIT '.$smart_from.','.$record_peage.'');
+    $req=$bds->prepare('SELECT  date,adresse,check_in,check_out,time,time1,clients,user,montant,montant_repas,mont_tva,types,id_fact,nombre,type,society,calls FROM facture WHERE email_ocd= :email_ocd AND code= :code ORDER BY id_fact DESC LIMIT '.$smart_from.','.$record_peage.'');
     $req->execute(array(':code'=>$session,
 	                   ':email_ocd'=>$_SESSION['email_ocd']));
 		
@@ -125,15 +125,15 @@ $smart_from =($page -1)*$record_peage;
 		
 		if($donns['permission']=="user:boss"){
 			
-			$calls='<span class="boss">'.$donns['call'].'</span>';
+			$calls='<span class="boss">'.$donnees['calls'].'</span>';
 		}
 		
 		if($donns['permission']=="user:gestionnaire"){
-			$calls='<span class="gestionnaire">'.$donns['call'].'</span>';
+			$calls='<span class="gestionnaire">'.$donnees['calls'].'</span>';
 		}
 		
 		if($donns['permission']=="user:employes"){
-			$calls='<span class="employe">'.$donns['call'].'</span>';
+			$calls='<span class="employe">'.$donnees['call'].'</span>';
 		}
 	
 	
