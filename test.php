055dbd33adda676eaeb190b997df3ca432282f78
zapo1234 <52051952+zapo1234@users.noreply.github.com>
2021-09-15 16:50:05 +0200

    Add files via upload

--- test.php
+++ test.php
@@ -37,16 +37,27 @@ if(isset($_POST['check'])){
 		$statement->execute();
 		
 		// suprimer dans la table home_occupation
-		$reg="DELETE FROM home_occupation WHERE code='".$valus."' AND id_fact ='".$vales."' AND email_ocd='".$email."'";
+		$reg="DELETE FROM home_occupation WHERE code='".$valus."' AND id_fact ='".$vales."' AND email_ocds='".$email."'";
 		$statement= $bds->prepare($reg);
-		$statement->execute();	
+		$statement->execute();
+
+        if(count($tab)==1){
+          echo'<div class="enre"><span class="d" style="color:#AB040E;"><i class="fas fa-exclamation-circle" style="font-size:16px;color:#AB040E;"></i> une facture suprimée</span></div>';
+		}
+
+        else{
+           $a=count($tab);
+		   
+		   echo'<div class="enre"><span class="d" style="color:#AB040E;"><i class="fas fa-exclamation-circle" style="font-size:16px;color:#AB040E;"></i>'.$a.' factures suprimées</span></div>';
+
+		}			
 	  
 	  }
 	}
    // suprimer dans la table home_occupation
   }
   else{
-	 echo'Martial'; 
+      echo'<div class="enre"><span class="d" style="color:#AB040E;"><i class="fas fa-exclamation-circle" style="font-size:16px;color:#AB040E;"></i> aucune facture selectionnée</span></div>';
   }
 
 ?>
