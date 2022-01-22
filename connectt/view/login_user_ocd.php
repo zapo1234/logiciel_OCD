4b440ad7f715725036cf0d0578c6e9c79de04c9e
zapo martial <52051952+zapo1234@users.noreply.github.com>
2022-01-22 14:36:07 +0100

    Add files via upload

---
+++ connectt/view/login_user_ocd.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1"/>
+     <link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico" />
+     <title>Optimisation de comptabilité à distance</title>
+	 
+	  <meta property="og:title" content="OCD - Optimisation de comptabilité à distance" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://www.ocdgestion.fr/" />
+    <meta property="og:description" content="Logiciel ERP innovant,Platforme qui met en relation instantanément patrons et gérants de caisse" />
+    <!-- TWITTER CARD -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="http://www.ocdgestion.fr/" />
+    <meta name="twitter:title" content="Solution innovante OCD en matiére de gestions d'activités commerciales" />
+    <meta name="twitter:description" content="suivire la mobilité de vos gérant,Optimisation de comptabilité à distance" />
+    <link rel="shortcut icon" href="" />
+	 
+   <!-- style CSS -->
+   <style type="text/css">
+
+@media (max-width: 575.98px) { ... }
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: 576px) and (max-width: 767.98px) { 
+
+
+
+}
+
+@media (min-width: 768px) and (max-width: 991.98px) { 
+
+
+}
+
+
+@media (min-width: 992px) and (max-width: 1199.98px) { 
+#close{width:100%;height:25px;background-color:white;}
+.hid{padding-left:4%;font-family:arial;font-size:12px;text-transform:uppercase;}
+.his{padding-left:20%;font-family:arial;font-size:12px;text-transform:uppercase;}
+}
+
+@media (min-width: 1200px) { 
+#close{width:100%;height:55px;background-color:white;padding-top:15px;}
+.hid{padding-left:4%;font-family:arial;font-size:12px;text-transform:uppercase;}
+.his{padding-left:20%;font-family:arial;font-size:12px;text-transform:uppercase;}
+#div_general{width:100%;} #espace{width:45%;height:500px;background-color:white;border:2px solid #eee;margin-top:30px;}
+form{padding:15%;} input{margin-top:15px;height:40px;font-family:arial;text-align:center;border:2px solid #eee;} .div{margin-top:25px;}
+.h{font-family:arial;font-size:16px;margin-top:-20px;color:black;font-weight:bold;}
+.div1{margin-top:25px;font-family:arial;font-size:13px;}
+#val{margin-top:25px;width:305px;height:40px;background-color:#00BFFF;border-bottom:2px solid #00BFFF;border-left:2px solid #00BFFF;font-family:arial;font-size:13px;}
+.footer1,.footer2,.footer3{float:left;margin-left:10%;font-family:arial;font-size:13px;} #id{padding-left:4%;}
+#footer{width:100%;background-color:white;height:200px;margin-top:30px;} .h{font-size:12px;font-family:arial;margin-top:20px;text-transform:uppercase;color:grey} li{list-style:none;padding-top:7px;text-transform:capitalze;}
+}
+
+   
+   </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+     <script type="text/javascript" src="jquery.js"></script>
+    
+  </head>
+  <body style="background-color: rgb(249, 249, 250)">
+ 
+<div class="container" id="close">
+ <img src ="image/logo.jpg" alt="logo" width="90" height="30"> <span class="his" id="entreprise"><a href="entreprise_ocd.php">Entreprise OCD </a></span><span class="hid">Tarifs abonements </span>
+ <span class="hid">Espace de travail gestionnaire </span><span class="hid" id="assistance"><a href="assistance_logiciel_ocd.php">Assistance formation </a></span>
+ </div>
+ 
+ <div class="container" id="div_general">
+
+ <div class="container" id="espace">
+ <form method="post" id="actif" action="">
+ <div class="h">Se connecter à Votre espace gestionnaire</div>
+ 
+ <div class="div"> <input type="text" name="email_ocd" id ="email_ocd" placeholder="E-mail" size="45" height="35" required/></div>
+ <div class="div"> <input type="text" name="id_ocd" id ="id_ocd" placeholder="IDENTIFIANT OCD" size="45" height="35" required/></div>
+ <div class="div"><input type="submit" value="connexion" id="val"></div>
+ </form>
+ 
+ <div class="div1"><span class="mot">Identifiants oubliés?</span> <span class="id">Envoyer un E-mail ou contacter nous</span></div>
+ 
+ 
+</div>
+ 
+ </div><!-- div_general-->
+
+ <div id="footer">
+ <div class="footer1">
+ <div class="h">Utlisation de OCD </div>
+ <li>Activité Hotelerie et appartements</li>
+ <li>Logiciel ERP innovant</li>
+ <li>Suivi check in et check out clients</li>
+ <li>Gestion de trésorie intéreactif</li>
+ <li>réssoudre efficacement votre gestion</li>
+ <li>Réaliser efficacement vos comptabilités</li>
+
+ </div>
+ 
+ <div class="footer2">
+ <div class="h">Aspect juridiques</div
+ <li>confidentialité</li>
+ <li>conditions d'utilisation</li>
+ <li>Régles entreprises</li>
+ <li>Gestion de trésorie intéreactif</li>
+</div>
+ 
+<div class="footer3">
+ <div class="h">Logiciel  ERP innovant</div>
+ <li>Gestion hotelerie</li>
+ <li>Gestion tresorie (entrées/depenses)</li>
+ <li>Assistance et formations</li>
+ <li>Optimisation de comptabilité à distance</li>
+ <li> Nous contacter : +33651756198 <br/><br/>Cote d'ivoire Direction
+      +22507737373</li>
+ </div>
+ 
+ </div><!--footer-->
+<script type="javascript">
+
+
+
+</script>
+
+</body>
+</html>
+
+
+    
